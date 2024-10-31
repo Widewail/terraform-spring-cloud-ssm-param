d0e9870784485f33dbdc647c0f0839eda6c51add
Matt Scafidi-McGuire <matts@widewail.com>
2024-10-31 12:03:31 -0400

    Removed conflicting default tags

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 locals {
   app_name = (var.env == null || length(var.env) > 0) ? "${var.app}_${var.env}" : var.app
-  default_tags = {
-    env = var.env
-  }
 }
 
 resource "aws_ssm_parameter" "param" {
@@ -10,5 +7,5 @@ resource "aws_ssm_parameter" "param" {
   type = var.type
 
   value = var.value
-  tags = var.tags == null ? local.default_tags : var.tags
+  tags = var.tags
 }
